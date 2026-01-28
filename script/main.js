1e3fc80d6b82df19238781116804ce4ce620df8f
EfraimNascimento <60748643+EfraimNascimento@users.noreply.github.com>
2026-01-28 11:44:32 -0300

    Alterações

--- script/main.js
+++ script/main.js
@@ -36,3 +36,49 @@ navLinks.forEach(link => {
     }
   });
 });
+
+let btnColection = document.getElementById('btnCol');   
+btnColection.addEventListener('click', function(e){
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if(target.offsetTop != 0){
+        document.getElementById('menu-span').classList.add('icon-op');
+    }else{
+        document.getElementById('menu-span').classList.remove('icon-op');
+    }
+    if(target){
+        const headerHeight = document.querySelector('header').offsetHeight;
+        const targetPosition = target.offsetTop - headerHeight - 15;
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth'
+        });
+    }
+});
+
+// 1. Seleciona todas as seções que você quer monitorar
+const sections = document.querySelectorAll('.monitorada');
+
+// 2. Cria o observador
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // 3. Verifica se a seção está visível (isIntersecting)
+    
+    if (entry.isIntersecting) {
+      console.log('Seção visível:', entry.target.id);
+      
+      
+      // Opcional: Adicionar/remover classes ativas
+      sections.forEach(s => s.classList.remove('active'));
+      entry.target.classList.add('active');
+    }
+  });
+}, {
+  // Opções: 0.5 significa que 50% da seção precisa estar visível
+   threshold: .24
+});
+
+// 4. Aplica o observador a cada seção
+sections.forEach((section) => {
+  observer.observe(section);
+});
